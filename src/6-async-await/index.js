bcf24209d76a6476a09d5a6bcecd44b45681dd17
vicenterg9 <vicenterg9@gmail.com>
2022-06-21 10:45:29 +0200

    fixes: 5-promise

--- src/6-async-await/index.js
+++ src/6-async-await/index.js
@@ -4,10 +4,25 @@ readFile();
 
 async function readFile() {
   const scriptFile = process.argv[1];
-  try{
+  try {
     const fileContent = await fs.readFile(scriptFile);
+    copyFile(null, fileContent);
     console.log(fileContent.toString());
   } catch (err) {
     console.log(`Error: ${err} `);
   }
+}
+
+//Leer fichero y copiarlo.
+const scriptFile = process.argv[1];
+async function copyFile(err, fileContent) {
+  if (err) {
+    console.log(`Error: ${err}`);
+  } else {
+    const fileWrite = await fs.writeFile(`${scriptFile}.copy.txt`, fileContent, onWriteEnd);
+  }
+}
+
+function onWriteEnd() {
+  console.log("Archivo copiado");
 }
